9ee94fe192c7612a7962c9e79d6f0ed76262c307
thetisiboth <thetisiboth@gmail.com>
2018-03-14 15:05:07 +0100

    ajout d'une interface, et affichage dynamique

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,19 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+  this.results = fakeResults;
   }
-
+  results : Result[];
 }
+
+interface Result {
+  title: string;
+  author: string;
+  date: string;
+  image: string;
+}
+const fakeResults :Result[] = [
+  {title:'title1',author:'pelo1',date:'01/01/01',image:'path/img1.png'},
+  {title:'title2',author:'pelo2',date:'02/02/02',image:'path/img2.png'},
+  {title:'title3',author:'pelo3',date:'03/03/03',image:'path/img3.png'}
+];
