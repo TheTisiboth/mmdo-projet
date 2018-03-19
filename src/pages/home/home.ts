1469260a6053aca46fdc3f647134409698812e8d
thetisiboth <thetisiboth@gmail.com>
2018-03-19 17:22:16 +0100

    rien de special

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,9 +9,9 @@ import { NgModel } from '@angular/forms';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-  this.results = [];
+    this.results = [];
   }
-  results : Result[];
+  results: Result[];
 
   getItems(ev: any) {
     let val = ev.target.value;
@@ -19,8 +19,8 @@ export class HomePage {
       this.results = fakeResults;
       document.getElementById("noresult").style.display = "none";
     }
-    else{
-      this.results=[];
+    else {
+      this.results = [];
       document.getElementById("noresult").style.display = "block";
 
     }
@@ -33,40 +33,10 @@ interface Result {
   date: string;
   image: string;
 }
-const fakeResults :Result[] = [
-  {title:'title1',author:'pelo1',date:'01/01/01',image:'path/img1.png'},
-  {title:'title2',author:'pelo2',date:'02/02/02',image:'path/img2.png'},
-  {title:'title3',author:'pelo3',date:'03/03/03',image:'path/img3.png'}
+const fakeResults: Result[] = [
+  { title: 'title1', author: 'pelo1', date: '01/01/01', image: 'path/img1.png' },
+  { title: 'title2', author: 'pelo2', date: '02/02/02', image: 'path/img2.png' },
+  { title: 'title3', author: 'pelo3', date: '03/03/03', image: 'path/img3.png' }
 ];
 
-class SearchPage {
 
-  searchQuery: string = '';
-  items: string[];
-
-  constructor() {
-    this.initializeItems();
-  }
-
-  initializeItems() {
-    this.items = [
-      'Amsterdam',
-      'Bogota'
-    ];
-  }
-
-  getItems(ev: any) {
-    // Reset items back to all of the items
-    this.initializeItems();
-
-    // set val to the value of the searchbar
-    let val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
-  }
-}
