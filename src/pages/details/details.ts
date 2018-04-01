53b97ac38ff349a9dbc5ce1e1e74cf157f8db025
loic <souchon.loic@free.fr>
2018-04-01 10:00:09 +0200

    recup leo detail.ts

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -15,7 +15,6 @@ import {Result} from '../home/home';
 })
 export class DetailsPage {
 
-<<<<<<< HEAD
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.param = this.navParams.data;
   }
@@ -24,27 +23,4 @@ export class DetailsPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetailsPage');
   }
-=======
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
-		this.param = {
-			title: this.navParams.get("title"),
-			author: this.navParams.get("author"),
-			date: this.navParams.get("date"),
-			image: this.navParams.get("image")
-		};
-	}
-	param: Result;
-
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad DetailsPage');
-	}
-
-}
-
-interface Result {
-	title: string;
-	author: string;
-	date: string;
-	image: string;
->>>>>>> 5d6ddf18e59e9b1e0ff3970e157c708c819ace72
 }
